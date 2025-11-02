dcdb7bcdbaa3453611b611ff7407e6b4241188db
Kevin Eslava <kevinalexisesma@ufps.edu.co>
2025-11-02 15:01:29 -0500

    Feat(WebFlux): Chunked mode to drive against pressure

--- src/main/java/com/bolsadeideas/springboot/webflux/app/controllers/ProductoController.java
+++ src/main/java/com/bolsadeideas/springboot/webflux/app/controllers/ProductoController.java
@@ -51,4 +51,18 @@ public class ProductoController {
         model.addAttribute("titulo", "Listado de Productos");
         return "listar";
     }
+
+    @GetMapping("/listar-full")
+    public String listarFull(Model model) {
+        Flux<Producto> productos = productoDao.findAll()
+                .map(producto -> {
+                    producto.setNombre(producto.getNombre().toUpperCase());
+                    return producto;
+                })
+                .repeat(5000);
+
+        model.addAttribute("productos", productos);
+        model.addAttribute("titulo", "Listado de Productos");
+        return "listar";
+    }
 }
