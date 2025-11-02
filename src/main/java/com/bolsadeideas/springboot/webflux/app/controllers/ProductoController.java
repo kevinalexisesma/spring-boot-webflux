bc7ee9c00b3094fa2490fb1e4d14bec512709755
Kevin Eslava <kevinalexisesma@ufps.edu.co>
2025-11-02 13:35:46 -0500

    Feat(WebFlux): Creating reactive controller and view

---
+++ src/main/java/com/bolsadeideas/springboot/webflux/app/controllers/ProductoController.java
@@ -0,0 +1,27 @@
+package com.bolsadeideas.springboot.webflux.app.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.bolsadeideas.springboot.webflux.app.models.dao.ProductoDao;
+import com.bolsadeideas.springboot.webflux.app.models.documents.Producto;
+
+import reactor.core.publisher.Flux;
+
+@Controller
+public class ProductoController {
+
+    @Autowired
+    private ProductoDao productoDao;
+
+    @GetMapping({ "/listar", "/" })
+    public String listar(Model model) {
+        Flux<Producto> productos = productoDao.findAll();
+
+        model.addAttribute("productos", productos);
+        model.addAttribute("titulo", "Listado de Productos");
+        return "listar";
+    }
+}
