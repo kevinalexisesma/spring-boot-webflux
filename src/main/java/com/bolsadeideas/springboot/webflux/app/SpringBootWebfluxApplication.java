8948cb6806bfb4546b2717210e4c65aaf7a5f864
Kevin Eslava <kevinalexisesma@ufps.edu.co>
2025-11-02 13:15:56 -0500

    Feat(WebFlux): Changing database and adding creation date

--- src/main/java/com/bolsadeideas/springboot/webflux/app/SpringBootWebfluxApplication.java
+++ src/main/java/com/bolsadeideas/springboot/webflux/app/SpringBootWebfluxApplication.java
@@ -1,5 +1,7 @@
 package com.bolsadeideas.springboot.webflux.app;
 
+import java.util.Date;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -39,7 +41,10 @@ public class SpringBootWebfluxApplication implements CommandLineRunner {
 				new Producto("Bianchi Bicicleta", 70.89),
 				new Producto("HP Notebook Omen 17", 2500.89),
 				new Producto("Mica Cómoda 5 Cajones", 150.89), new Producto("TV Sony Bravia OLED 4K Ultra HD", 2255.89))
-				.flatMap(producto -> productoDao.save(producto))
+				.flatMap(producto -> {
+					producto.setCreateAt(new Date());
+					return productoDao.save(producto);
+				})
 				.subscribe(producto -> log.info("Insert: " + producto.getId() + " " + producto.getNombre()));
 	}
 
